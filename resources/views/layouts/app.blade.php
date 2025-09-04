6927a3540c4d2976e60536c319b24299576d5068
Chaimanat <65160215@go.buu.ac.th>
2025-09-04 14:43:15 +0700

    ✨feature: guest page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,10 @@
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" />
     <script src="https://cdn.tailwindcss.com"></script>
 
+    {{-- Favicon --}}
+    <link rel="icon" type="image/png" href="{{ asset('image/web-image/logo.png') }}">
+    <link rel="shortcut icon" href="{{ asset('image/web-image/logo.png') }}">
+
     {{-- ฟอนต์ Kanit --}}
     <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;600&display=swap" rel="stylesheet">
 
@@ -28,7 +32,7 @@
     </style>
 </head>
 
-@if (Auth::check() && Auth::user()->role === 'jobber')
+@if (Auth::check() && Auth::user()->role === 'jobber' || !Auth::check())
 
 <body class="h-full min-h-screen bg-[linear-gradient(to_bottom,_theme('colors.sky.100')_20%,_theme('colors.blue.300')_100%)] px-28">
     <div class="flex">
@@ -45,7 +49,7 @@
     </div>
 </body>
 @else
-<body class="h-full min-h-screen p-6 bg-gray-200">
+<body class="h-full min-h-screen px-6 bg-gray-200">
     <div class="flex">
         {{-- Sidebar --}}
         @include('layouts.sidebar')
