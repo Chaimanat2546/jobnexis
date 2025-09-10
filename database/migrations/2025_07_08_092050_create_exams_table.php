c5a428cb778c0fe8c5a314e9852025fc4b7c7b46
Chaimanat <65160215@go.buu.ac.th>
2025-09-10 11:09:22 +0700

    🐞 fix: exam table

--- database/migrations/2025_07_08_092050_create_exams_table.php
+++ database/migrations/2025_07_08_092050_create_exams_table.php
@@ -14,10 +14,11 @@ return new class extends Migration
         Schema::create('exams', function (Blueprint $table) {
             $table->bigIncrements('e_id');
             $table->string('e_name');
-            $table->text('e_description')->nullable(); // เปลี่ยนจาก string เป็น text
+            $table->text('e_description')->nullable();
             $table->foreignId('e_l_id')->nullable()->references('l_id')->on('lessons')->onDelete('cascade'); // เพิ่ม nullable()
             $table->foreignId('e_c_id')->references('c_id')->on('courses')->onDelete('cascade'); // เพิ่ม FK คอร์ส
             $table->integer('e_index')->default(0); // เพิ่มลำดับ
+            $table->json('e_skills')->nullable()->after('e_description');
             $table->timestamps();
         });
     }
